4deaf50f926448c4785b010cc52549bf928fc3c5
Tanay Jalan <tanayjalan@gmail.com>
2013-07-07 21:18:09 -0400

    getting app ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfficeClassifieds::Application.config.secret_token = '4b9ade2be9e277e122a7c981c95aefad0fb1c3442977a2db30c9a1443a1fb875b9dcf953c972e5cd7cf39650074a2111dea5d3d1f80fcb27b10aa3194fdba068'
