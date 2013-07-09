22b2673b0d08b066de80550cc2c66aef91083d1b
Lee Hampton <leejhampton@gmail.com>
2013-07-09 11:23:52 -0400

    convert neighborhoods to member route, update tests

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,6 +1,7 @@
 class NeighborhoodsController < ApplicationController
   def index
     @neighborhoods = Neighborhood.all
+    @city = City.find(params[:city_id])
   end
 
   def show
--- spec/features/neighborhood_spec.rb
+++ spec/features/neighborhood_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
-describe 'Neighborhood' do
+describe 'Neighborhoods' do
   let(:neighborhood) { create(:neighborhood) } 
   let(:neighborhood_2) { create(:neighborhood_2) } 
   let(:neighborhood_3) { create(:neighborhood_3) } 
+  let(:city) { create(:city) }
 
-  describe 'GET /neighborhoods' do
+  describe 'GET cities/:city_id/neighborhoods' do
     it 'displays all of the neighborhoods' do
       neighborhood.reload
       neighborhood_2.reload
       neighborhood_3.reload
-      visit neighborhoods_path
+      city.reload
+      visit city_neighborhoods_path(city)
       page.should have_selector('div', text: neighborhood.name)
       page.should have_selector('div', text: neighborhood_2.name)
       page.should have_selector('div', text: neighborhood_3.name)
@@ -20,15 +22,16 @@ describe 'Neighborhood' do
       neighborhood.reload
       neighborhood_2.reload
       neighborhood_3.reload
-      visit neighborhoods_path
+      city.reload
+      visit city_neighborhoods_path(city)
       click_link(neighborhood.name)
-      current_path.should eq(neighborhood_path(neighborhood))
-      visit neighborhoods_path
+      current_path.should eq(city_neighborhood_path(city, neighborhood))
+      visit city_neighborhoods_path(city)
       click_link(neighborhood_2.name)
-      current_path.should eq(neighborhood_path(neighborhood_2))
-      visit neighborhoods_path
+      current_path.should eq(city_neighborhood_path(city, neighborhood_2))
+      visit city_neighborhoods_path(city)
       click_link(neighborhood_3.name)
-      current_path.should eq(neighborhood_path(neighborhood_3))
+      current_path.should eq(city_neighborhood_path(city, neighborhood_3))
     end
   end
 end
