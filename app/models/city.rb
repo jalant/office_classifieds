7070a3cca99a3d18b809f97a250a3609bea7b7d7
Tanay Jalan <tanayjalan@gmail.com>
2013-07-08 22:16:13 -0400

    city model all tested, moving on to city controller

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,8 @@
 class City < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :img
+
+  validates_presence_of :name, :img
+
+  has_many :neighborhoods 
+  has_many :office_listings, through: :neighborhoods
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709005417) do
+ActiveRecord::Schema.define(:version => 20130709020828) do
 
   create_table "brokers", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20130709005417) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "img"
   end
 
   create_table "favorites", :force => true do |t|
