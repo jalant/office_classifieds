a0e48056b4ca92278f84684f41c3b5871da5e6a8
Tanay Jalan <tanayjalan@gmail.com>
2013-07-30 23:05:19 -0400

    model fixing tests

--- spec/models/renter_spec.rb
+++ spec/models/renter_spec.rb
@@ -28,37 +28,9 @@ describe Renter do
   subject(:renter) {create(:renter)}
 
   describe 'validation' do
-    it 'does not require a first name' do
-    	pending
-      expect(subject).to be_valid
-      subject.first_name = nil
-      expect(subject).to be_valid
-    end
-    it 'does not require a last name' do
-    	pending
-      expect(subject).to be_valid
-      subject.last_name = nil
-      expect(subject).to be_valid
-    end	
-    it 'does not require a phone number' do
-    	pending
-      expect(subject).to be_valid
-      subject.phone_number = nil
-      expect(subject).to be_valid
-    end
-    it 'does not require an img_url' do
-    	pending
-      expect(subject).to be_valid
-      subject.img_url = nil
-      expect(subject).to be_valid
-    end
-    it 'does not require an address' do
-    	pending
-      expect(subject).to be_valid
-      subject.address = nil
-      expect(subject).to be_invalid
-    end
-
+    
+  
+ 
     it 'requires a email' do
       expect(subject).to be_valid
       subject.email = nil
@@ -75,11 +47,6 @@ describe Renter do
       expect(subject).to be_invalid
     end
 
-    it 'requires address to be less than 80 characters' do
-      expect(subject).to be_valid
-      subject.address = '1207 Dover Road, Singaproe, Singapore, Singapore, Singapore, Singapore, Singapore, Singapore, Singapore, Singapore'
-      expect(subject).to be_invalid
-    end
   end
 
   describe 'associations' do
