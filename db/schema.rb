a62ce0e0cc1a8d169b248c7b4ee6780573d29244
David Aaron <davidmatthewaaron@gmail.com>
2013-07-30 21:55:27 -0400

    database config changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,23 +9,26 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713032402) do
+ActiveRecord::Schema.define(version: 20130713032402) do
 
-  create_table "brokers", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "brokers", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
     t.text     "address"
@@ -34,92 +37,92 @@ ActiveRecord::Schema.define(:version => 20130713032402) do
     t.string   "firm"
   end
 
-  add_index "brokers", ["email"], :name => "index_brokers_on_email", :unique => true
-  add_index "brokers", ["reset_password_token"], :name => "index_brokers_on_reset_password_token", :unique => true
+  add_index "brokers", ["email"], name: "index_brokers_on_email", unique: true, using: :btree
+  add_index "brokers", ["reset_password_token"], name: "index_brokers_on_reset_password_token", unique: true, using: :btree
 
-  create_table "cities", :force => true do |t|
+  create_table "cities", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "img"
   end
 
-  create_table "favorites", :force => true do |t|
+  create_table "favorites", force: true do |t|
     t.integer  "renter_id"
     t.integer  "office_listing_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "images", :force => true do |t|
+  create_table "images", force: true do |t|
     t.text     "image"
     t.integer  "office_listing_id"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "link_image"
   end
 
-  create_table "neighborhoods", :force => true do |t|
+  create_table "neighborhoods", force: true do |t|
     t.string   "name"
     t.integer  "city_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "img_url"
     t.float    "latitude"
     t.float    "longitude"
     t.text     "address"
   end
 
-  create_table "notifications", :force => true do |t|
+  create_table "notifications", force: true do |t|
     t.string   "subject"
     t.text     "body"
     t.integer  "office_listing_id"
     t.boolean  "read"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "broker_id"
     t.integer  "renter_id"
   end
 
-  create_table "office_listings", :force => true do |t|
+  create_table "office_listings", force: true do |t|
     t.string   "address"
     t.string   "office_type"
     t.integer  "size"
     t.integer  "rent"
     t.text     "details"
-    t.boolean  "kitchen",                :default => false
-    t.boolean  "reception",              :default => false
-    t.boolean  "light",                  :default => false
-    t.boolean  "shower",                 :default => false
-    t.boolean  "move_in",                :default => false
-    t.boolean  "high_ceiling",           :default => false
-    t.boolean  "patio",                  :default => false
-    t.boolean  "furniture",              :default => false
+    t.boolean  "kitchen",                default: false
+    t.boolean  "reception",              default: false
+    t.boolean  "light",                  default: false
+    t.boolean  "shower",                 default: false
+    t.boolean  "move_in",                default: false
+    t.boolean  "high_ceiling",           default: false
+    t.boolean  "patio",                  default: false
+    t.boolean  "furniture",              default: false
     t.string   "term_length"
     t.string   "availability"
     t.integer  "no_of_offices"
     t.integer  "no_of_conference_rooms"
     t.integer  "neighborhood_id"
     t.integer  "broker_id"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.float    "latitude"
     t.float    "longitude"
   end
 
-  create_table "renters", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "renters", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
     t.text     "address"
@@ -127,7 +130,7 @@ ActiveRecord::Schema.define(:version => 20130713032402) do
     t.string   "img_url"
   end
 
-  add_index "renters", ["email"], :name => "index_renters_on_email", :unique => true
-  add_index "renters", ["reset_password_token"], :name => "index_renters_on_reset_password_token", :unique => true
+  add_index "renters", ["email"], name: "index_renters_on_email", unique: true, using: :btree
+  add_index "renters", ["reset_password_token"], name: "index_renters_on_reset_password_token", unique: true, using: :btree
 
 end
